f8700f25c959f3f1cb332b93e3d0976a9f19f00b
zzm <330777196@qq.com>
2019-11-05 11:47:53 +0800

    配置antd按需加载

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from 'react'
-
-function App() {
-    return <div className="App"></div>
+import React, { Component } from 'react'
+import { Button } from 'antd'
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+    render() {
+        return <div></div>
+    }
 }
 
 export default App
