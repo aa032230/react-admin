bf2fbf77e759a5980c22958c0816b8778e152ca2
zzm <330777196@qq.com>
2019-11-07 23:56:34 +0800

    '配置'

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,14 @@
 const { override, fixBabelImports, addLessLoader } = require('customize-cra')
 
 module.exports = override(
+    // 将antd的组件进行懒加载
     fixBabelImports('import', {
         libraryName: 'antd',
         libraryDirectory: 'es',
         style: true
     }),
+
+    // 更换antd主题
     addLessLoader({
         javascriptEnabled: true,
         modifyVars: { '@primary-color': '#1DA57A' }
